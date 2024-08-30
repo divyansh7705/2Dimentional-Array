16ce55aa01d142e82c7202453f06dd56f79b1355
divyansh7705 <divyanshrastogi91@gmail.com>
2024-08-31 00:54:52 +0530

    Add files via upload

---
+++ diagonal.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+     int n = 3;
+    int a[n][n] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+if(n != n){
+     cout << "addition not possible" << endl;
+}
+     
+    int diagonalSum = 0;
+     for (int i = 0; i < n; i++) {
+        diagonalSum += a[i][i];
+        
+    }
+    
+    for(int i = 0;i<3;i++){
+        for(int j = 0; j<3;j++){
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "Diagonal Sum: " << diagonalSum << endl;
+   
+
+    return 0;
+}
---
+++ transpose.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+    const int n = 3, m = 2;
+
+    int A[n][m] = {
+    {1, 2},{3, 4},{5, 6}
+    };
+
+
+    int T[m][n];
+    
+    cout << "original: " << endl;
+    for(int i = 0;i<n;i++){
+        for(int j = 0;j<m;j++){
+            cout << A[i][j] << " ";
+            
+        }
+        cout << endl;
+    }
+cout << "transpose: "  << endl;
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < m; j++)
+            T[j][i] = A[i][j];
+
+    for (int i = 0; i < m; i++) {
+       for (int j = 0; j < n; j++)
+            cout << T[i][j] << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
